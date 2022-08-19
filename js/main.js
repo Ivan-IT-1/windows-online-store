cc532fd615e8d938ab1bc453357c560da93502f3
Ivan-IT-1 <108977968+Ivan-IT-1@users.noreply.github.com>
2022-08-19 12:27:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let switchMode = document.getElementById("switchMode");
+
+switchMode.onclick = function () {
+    let theme = document.getElementById("theme");
+
+    if(theme.getAttribute("href") == "css/light-mode.css" ){
+        theme.href = "css/dark-mode.css"
+    } else {
+        theme.href = "css/light-mode.css"
+    }
+}
